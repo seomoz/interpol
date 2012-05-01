18c97aa4308d23d4d98fcf9d19343e9d1e9cd35f
Myron Marston <myron.marston@gmail.com>
2012-05-01 14:48:58 -0700

    Failing spec demonstrating array bug.

--- spec/unit/interpol/documentation_spec.rb
+++ spec/unit/interpol/documentation_spec.rb
@@ -3,56 +3,51 @@ require 'interpol/documentation'
 
 module Interpol
   describe Documentation do
-    describe ".html_for_schema" do
-      let(:schema) do
-        {"description"=>"Returns information about a contact.",
+    let(:address_schema) do
+      {"first_name"=>
+        {"description"=>"The person's first name.",
+         "type"=>"string",
+         "additionalProperties"=>false,
+         "required"=>true},
+       "last_name"=>
+        {"description"=>"The person's last name.",
+         "type"=>"string",
+         "additionalProperties"=>false,
+         "required"=>true},
+       "date_of_birth"=>
+        {"description"=>"The person's birthday.",
+         "type"=>"date",
+         "additionalProperties"=>false,
+         "required"=>true},
+       "gender"=>
+        {"description"=>"Male or Female",
+         "type"=>"string",
+         "enum"=>["male", "female"],
+         "additionalProperties"=>false,
+         "required"=>true},
+       "address"=>
+        {"description"=>"The person's mailing address",
          "type"=>"object",
          "properties"=>
-          {"first_name"=>
-            {"description"=>"The person's first name.",
+          {"street"=>
+            {"description"=>"Street address",
              "type"=>"string",
              "additionalProperties"=>false,
              "required"=>true},
-           "last_name"=>
-            {"description"=>"The person's last name.",
+           "city"=>
+            {"type"=>"string", "additionalProperties"=>false, "required"=>true},
+           "state"=>
+            {"type"=>"string", "additionalProperties"=>false, "required"=>true},
+           "zip"=>
+            {"description"=>"Zip code",
              "type"=>"string",
-             "additionalProperties"=>false,
-             "required"=>true},
-           "date_of_birth"=>
-            {"description"=>"The person's birthday.",
-             "type"=>"date",
-             "additionalProperties"=>false,
-             "required"=>true},
-           "gender"=>
-            {"description"=>"Male or Female",
-             "type"=>"string",
-             "enum"=>["male", "female"],
-             "additionalProperties"=>false,
-             "required"=>true},
-           "address"=>
-            {"description"=>"The person's mailing address",
-             "type"=>"object",
-             "properties"=>
-              {"street"=>
-                {"description"=>"Street address",
-                 "type"=>"string",
-                 "additionalProperties"=>false,
-                 "required"=>true},
-               "city"=>
-                {"type"=>"string", "additionalProperties"=>false, "required"=>true},
-               "state"=>
-                {"type"=>"string", "additionalProperties"=>false, "required"=>true},
-               "zip"=>
-                {"description"=>"Zip code",
-                 "type"=>"string",
-                 "additionalProperties"=>false,
-                 "required"=>true}},
              "additionalProperties"=>false,
              "required"=>true}},
          "additionalProperties"=>false,
-         "required"=>true}
-      end
+         "required"=>true}}
+    end
 
+    shared_examples_for "schema rendering" do
       let(:html) { Documentation.html_for_schema(schema) }
       let(:parsed_html) { Nokogiri::HTML::DocumentFragment.parse(html) }
 
@@ -82,6 +77,33 @@ module Interpol
           "state (string)",
           "zip (string)"
         ])
+      end
+    end
+
+    describe ".html_for_schema" do
+      context "for an array schema" do
+        let(:schema) do
+          {"description"=>"Returns information about a contact.",
+           "type"=>"array",
+           "items"=>
+            {"properties"=>address_schema,
+             "additionalProperties"=>false,
+             "required"=>true} }
+        end
+
+        it_behaves_like "schema rendering"
+      end
+
+      context "for an object schema" do
+        let(:schema) do
+          {"description"=>"Returns information about a contact.",
+           "type"=>"object",
+           "properties"=>address_schema,
+           "additionalProperties"=>false,
+           "required"=>true}
+        end
+
+        it_behaves_like "schema rendering"
       end
     end
   end
