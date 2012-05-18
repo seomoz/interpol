11a5fa538b8a3bb0ef7b4ca7a0ad710fd04967de
Evan Battaglia <evan@seomoz.org>
2012-05-17 17:05:58 -0700

    Super-minor changes based on Myron's comments

--- spec/simplecov_setup.rb
+++ spec/simplecov_setup.rb
@@ -1,4 +1,5 @@
-if defined?(RUBY_ENGINE) && RUBY_ENGINE == 'ruby' # MRI only
+# MRI only
+if defined?(RUBY_ENGINE) && RUBY_ENGINE == 'ruby' && RUBY_VERSION.to_f >= 1.9
   require 'simplecov'
 
   SimpleCov.start do
