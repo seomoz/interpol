bff7ef57c37f58fb81d564722fe8f0e590ebd401
Myron Marston <myron.marston@gmail.com>
2012-05-24 14:22:50 -0700

    Ensure assets are served relative to mounted doc app.

--- lib/interpol/documentation_app.rb
+++ lib/interpol/documentation_app.rb
@@ -77,6 +77,15 @@ module Interpol
       def title
         interpol_config.documentation_title
       end
+
+      def url_path(*path_parts)
+        [ path_prefix, path_parts ].join("/").squeeze('/')
+      end
+      alias_method :u, :url_path
+
+      def path_prefix
+        request.env['SCRIPT_NAME']
+      end
     end
 
     # Private: Builds a stub sinatra app for the given interpol
